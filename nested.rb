8e430c5a7b3f95dd94200aebdc75aef44153588b
Nethelbert <gilblasse@gmail.com>
2019-11-07 11:49:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+<<<<<<< HEAD
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
+=======
+programmer_hash[:dennis_ritchie][]
+>>>>>>> 21e9f07c1273e328e47142d6c7ff4f46fab3d8c4
 end
